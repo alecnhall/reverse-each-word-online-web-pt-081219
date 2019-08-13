b4e864fa8b10c091d6707ac6ac464d0ea950186d
Alec Hall <alecnhall@gmail.com>
2019-08-13 19:57:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ reverse_string = "Hello there, and how are you?"
 
 def reverse_each_word(string)
   new_array = string.split(" ")
-  return new_array
+  p new_array
 end
 
 reverse_each_word(reverse_string)
