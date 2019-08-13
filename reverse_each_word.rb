83a09598551226af11cd1fb62ab5e1dbef0d1581
Alec Hall <alecnhall@gmail.com>
2019-08-13 20:00:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@ reverse_string = "Hello there, and how are you?"
 def reverse_each_word(string)
   new_array = string.split(" ")
   new_array.each do |word|
-    p word.reverse
+    word.reverse
+    p new_array.join(" ")
   end
 end
 
