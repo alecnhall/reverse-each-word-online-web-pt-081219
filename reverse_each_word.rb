40cfad33db9bd532e489e83e30aac9abb3a26dea
Alec Hall <alecnhall@gmail.com>
2019-08-13 20:06:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,12 @@ def reverse_each_word(string)
 end
 
 reverse_each_word(reverse_string)
+
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  reverse_array = []
+  new_array.collect do |word|
+    reverse_array.push(word.reverse)
+  end
+  return reverse_array.join(" ")
+end
